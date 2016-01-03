b7f290e08a6884f58701068251c546103fa37242
Karol Kowalczyk <karolatwp@wp.pl>
2016-01-03 03:08:56 +0100

    logowanie

--- readme.txt
+++ readme.txt
@@ -3,11 +3,10 @@ https://spring.io/blog/2014/12/02/latest-jackson-integration-improvements-in-spr
 http://docs.spring.io/autorepo/docs/spring-android/1.0.x/reference/html/rest-template.html
 http://stackoverflow.com/questions/23674046/get-list-of-json-objects-with-spring-resttemplate
 
-#build
+#build and test by maven 3.x with java 1.7
 mvn -U clean package
 
-#test
-mvn -U clean test
-
 #run
-java -jar target\GoEuroTest.jar Warszawa
+cd target
+java -jar GoEuroTest.jar Warszawa
+more  Warszawa.csv
--- src/main/java/pl/noriSoftware/goEuro/GoEuroTest.java
+++ src/main/java/pl/noriSoftware/goEuro/GoEuroTest.java
@@ -8,18 +8,26 @@ import java.io.IOException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author karol
  *
  */
 public class GoEuroTest {
 
+	private static final Class<GoEuroTest> thisClass = GoEuroTest.class;
+	final static protected Logger logger = LoggerFactory.getLogger(thisClass.getName());
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		if (args.length < 1) {
-			throw new RuntimeException("Expected city name parameter to run for example: java -jar GoEuroTest.jar \"New York\"");
+			RuntimeException ex =new RuntimeException("Expected city name parameter to run for example: java -jar GoEuroTest.jar \"New York\"");
+			logger.error("error", ex);
+			throw ex;
 		}
 		@SuppressWarnings("resource")
 		final ApplicationContext context = new ClassPathXmlApplicationContext(
@@ -28,8 +36,9 @@ public class GoEuroTest {
 				.getBean("springBean.City2CSV");
 		try {
 			city2CSV.run(args[0]);
-		} catch (IOException e) {
-			e.printStackTrace();
+		} catch (IOException ex) {
+			logger.error("error", ex);
+			ex.printStackTrace();
 		}
 	}
 
